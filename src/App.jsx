3a448928dbd7e9267c1243c57d1d7f79fbd64f12
Lauren Clark <lauren.c4231@gmail.com>
2021-05-02 22:23:00 +0100

    Scaffold app and logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import { GridContainer } from './AppStyles';
 
 function App() {
     const fieldsQuantity = 4;
-    const [data, setData] = useState[{}];
+    const [currentPosition, setCurrentPosition] = useState(0);
+    const [template, setTemplate] = useState({});
+    const [data, setData] = useState([
+        {
+            id: '1',
+            text: 'There once was a big bad wolf',
+            fields: ['', '', '', ''],
+        },
+    ]);
 
-    useEffect(() => {
-        setData([
-            ...data,
-            {
-                id: 1,
-                text: 'Wolfie',
-                fields: ['Something', 'sdf', 'sdfsdf', 'sdfds'],
-            },
-        ]);
-        return () => {};
-    }, []);
+    function handleSubmit(index, field) {
+        // Add the field to the correct index in fields array (Set?)
+    }
+
+    function handleTextClick(text) {
+        // Add the text to the text property in template
+        // Move to the next item in array to progress linear path
+    }
+
+    function computeClass(className, index) {
+        return String(`${className}-${index + 1}`);
+    }
 
     return (
         <div>
-            {data.length > 1 &&
-                data.map((item) => {
-                    <pre>{item.id}</pre>;
-                })}
+            {data &&
+                data.map(
+                    ({ id, text, fields }, index) =>
+                        index === currentPosition && (
+                            <GridContainer key={id}>
+                                <div className="cell-0">{text}</div>
+                                {fields.map((field, index) => {
+                                    if (field) {
+                                        return (
+                                            <div
+                                                className={computeClass(
+                                                    'cell',
+                                                    index,
+                                                )}
+                                                key={field + index}
+                                            >
+                                                <a href="#">{field}</a>
+                                            </div>
+                                        );
+                                    }
+                                    return (
+                                        <div
+                                            className={computeClass(
+                                                'cell',
+                                                index,
+                                            )}
+                                            key={field + index}
+                                        >
+                                            <input type="text" />
+                                            <button>Submit</button>
+                                        </div>
+                                    );
+                                })}
+                            </GridContainer>
+                        ),
+                )}
         </div>
     );
 }
