a93a1ecd1c9d32e30110bc6c4e83bcd6b959b461
Lauren Clark <lauren.c4231@gmail.com>
2021-05-02 22:23:05 +0100

    Add styles

---
+++ src/AppStyles.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+const GridContainer = styled.div`
+    @media (min-width: 769px) {
+        display: grid;
+        grid-template-rows: 2fr 1fr 2fr;
+        grid-gap: 2rem;
+        max-width: 800px;
+        margin: auto;
+        place-content: center;
+        place-items: center;
+        .cell-0 {
+            grid-column: 2;
+            grid-row: 2;
+            order: 3;
+        }
+        .cell-1 {
+            grid-column: 2;
+            grid-row: 1;
+            order: 1;
+        }
+        .cell-2 {
+            grid-column: 1;
+            grid-row: 2;
+            order: 2;
+        }
+        .cell-3 {
+            grid-column: 2;
+            grid-row: 3;
+            order: 4;
+        }
+        .cell-4 {
+            grid-column: 3;
+            grid-row: 2;
+            order: 2;
+        }
+    }
+`;
+
+export { GridContainer };
